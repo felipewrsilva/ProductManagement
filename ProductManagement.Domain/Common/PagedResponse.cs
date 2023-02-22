6d6f869cdc1aa350efb5413d53ec3f0d54197abd
Felipe Silva <felipewrsilva@gmail.com>
2023-02-22 11:29:48 -0300

    Refactor PagedResponse class to include a static Create method that sets TotalPages property

--- ProductManagement.Domain/Common/PagedResponse.cs
+++ ProductManagement.Domain/Common/PagedResponse.cs
@@ -7,7 +7,18 @@ namespace ProductManagement.Domain.Common
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
         public long TotalItems { get; set; }
-        public int TotalPages { get; set; }
         public IEnumerable<T> Items { get; set; }
+        public object TotalPages { get; set; }
+
+        public static PagedResponse<T> Create(IEnumerable<T> items, long totalItems, int pageNumber, int pageSize)
+        {
+            return new PagedResponse<T>
+            {
+                Items = items,
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalItems = totalItems
+            };
+        }
     }
 }
