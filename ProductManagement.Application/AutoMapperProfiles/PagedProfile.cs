549465d576c29f87626b6bf8a3321b1b0b1d8780
Felipe Silva <felipewrsilva@gmail.com>
2023-02-22 11:25:58 -0300

    Add AutoMapper profile for PagedResult to PagedResponse mapping.

---
+++ ProductManagement.Application/AutoMapperProfiles/PagedProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using ProductManagement.Domain.Common;
+
+namespace ProductManagement.Application.AutoMapperProfiles
+{
+    public class PagedProfile : Profile
+    {
+        public PagedProfile()
+        {
+            CreateMap(typeof(PagedResult<>), typeof(PagedResponse<>));
+        }
+    }
+}
