d2d29b4f8917c819e621dda1dff67b7d34069700
Felipe Silva <felipewrsilva@gmail.com>
2023-02-22 11:31:57 -0300

    Refactor PagedResponse to PagedResult and update property names and types for clarity and consistency.

--- ProductManagement.Domain/Common/PagedResult.cs
+++ ProductManagement.Domain/Common/PagedResult.cs
@@ -4,17 +4,20 @@ namespace ProductManagement.Domain.Common
 {
     public class PagedResult<T>
     {
-        public int PageIndex { get; set; }
+        public int PageNumber { get; set; }
         public int PageSize { get; set; }
-        public long TotalCount { get; set; }
-        public IReadOnlyList<T> Items { get; set; }
+        public long TotalItems { get; set; }
+        public IEnumerable<T> Items { get; set; }
 
-        public PagedResult(int pageIndex, int pageSize, long totalCount, IReadOnlyList<T> items)
+        public static PagedResult<T> Create(IEnumerable<T> items, long totalItems, int pageNumber, int pageSize)
         {
-            PageIndex = pageIndex;
-            PageSize = pageSize;
-            TotalCount = totalCount;
-            Items = items;
+            return new PagedResult<T>
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalItems = totalItems,
+                Items = items
+            };
         }
     }
 }
